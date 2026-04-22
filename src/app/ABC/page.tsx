9a5ad8339049d8315de1d1a558d9ef8e08b7eb8d
Sierra <mac@Satriyas-Macbook.local>
2026-04-23 00:21:14 +0800

    initial commit

---
+++ src/app/ABC/page.tsx
@@ -0,0 +1,9 @@
+export default function ABC() {
+  return (
+    <div>
+        <p> ABCDEFG Just like in the kindergarten</p>
+        <input type="text" placeholder="What's on your mind?" />
+        <button> Click Me!</button>
+    </div>
+  );
+}
---
+++ src/app/Blog/page.tsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+  return (
+    <div>
+        <p> Howdy, Im testing this page</p>
+        <input type="text" placeholder="What's on your mind?" />
+        <button> Click Me!</button>
+    </div>
+  );
+}
