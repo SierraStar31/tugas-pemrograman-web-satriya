9735a164b7582cbfc137fc395e320741ee5fdd3b
Satriya Yogananda <satriayogaaa@gmail.com>
2026-04-29 22:10:36 +0800

    belajar design

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,14 +1,39 @@
 export default function Learn() {
+  // Function dari JS
   return (
-    <div>
-        <div>
-            <img  />
-            <p> Design </p>
+    <div className="bg-white h-screen flex text-blue-950 flex gap-4 items-center justify-center">
+      <div className="bg-purple-300 h-72 w-96 p-8 flex flex-col justify-between rounded-2xl">
+        <div className=" flex items-center gap-4">
+          <img src={"/palette.svg"} alt=" Icon Palette" />
+          <p> Design </p>
         </div>
         <div>
-            <h1> Figma </h1>
-            <p> 8 hours ago </p>
+          <h1 className="text-4xl font-bold"> Figma </h1>
+          <p> 8 hours ago </p>
         </div>
+      </div>
+
+      <div className="bg-orange-300 h-72 w-96 p-8 flex flex-col justify-between rounded-2xl">
+        <div className=" flex items-center gap-4">
+          <img src={"/palette.svg"} alt=" Icon Palette" />
+          <p> Python </p>
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold"> Python </h1>
+          <p> 8 hours ago </p>
+        </div>
+      </div>
+
+      <div className="bg-red-300 h-72 w-96 p-8 flex flex-col justify-between rounded-2xl">
+        <div className=" flex items-center gap-4">
+          <img src={"/palette.svg"} alt=" Icon Palette" />
+          <p> Sketch </p>
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold"> Sketch </h1>
+          <p> 8 hours ago </p>
+        </div>
+      </div>
     </div>
   );
 }
